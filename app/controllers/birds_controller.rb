c16010ad05e0ae6dde51fe01566db774380cef2e
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-12-08 22:06:25 -0800

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,11 +1,9 @@
 class BirdsController < ApplicationController
-
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
   end
-
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -15,5 +13,10 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+  # POST /birds
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
-Rails.application.routes.draw do
-  resources :birds, only: [:index, :show]
-end
+# Rails.application.routes.draw do
+#   resources :birds, only: [:index, :show]
+# end
+
+Rails.application.routes.draw do #1=> Creating new birds. Use resources method, we can create this route by adding in create to the list of actions we want handled:
+  resources :birds, only: [:index, :show, :create]
+end 
+
+#1=> After updating this route, run rails routes to check what routes are now available. 
+# Prefix Verb URI Pattern          Controller#Action
+#  birds GET  /birds(.:format)     birds#index
+#        POST /birds(.:format)     birds#create
+#   bird GET  /birds/:id(.:format) birds#show
